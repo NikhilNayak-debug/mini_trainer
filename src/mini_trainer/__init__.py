d094d89de7e46cd0fc06a1bee7a34f4573990d92
Oleg Silkin <97077423+RobotSail@users.noreply.github.com>
2025-08-18 23:51:01 +0000

    move data processing script into scripts

--- src/mini_trainer/__init__.py
+++ src/mini_trainer/__init__.py
@@ -11,7 +11,6 @@ from . import async_structured_logger
 from . import batch_metrics
 from . import batch_packer
 from . import none_reduction_losses
-from . import process_data
 from . import sampler
 from . import setup_model_for_training
 from . import svd_utils
@@ -27,7 +26,6 @@ __all__ = [
     "batch_metrics", 
     "batch_packer",
     "none_reduction_losses",
-    "process_data",
     "sampler",
     "setup_model_for_training",
     "svd_utils",
